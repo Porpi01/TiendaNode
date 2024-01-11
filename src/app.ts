2fba7e94568752c45f86d922fe1caa9541e661b6
monica <macanyiz@gmial.com>
2024-01-11 08:56:28 +0100

    cambios

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("Hi Elena");
+import express from "express";
+
+console.log("-------------------------");
+console.log("Bienvenido a mi app");
+
+const port = 3000; //Puerto para recibir peticiones
+
+const app = express(); //Nos trae la app
+
+//Controladores para responder a las peticiones por HTTP
+
+// FIN
+app.listen(port); //Poner en marcha la app
+console.log("Servidor de la app en marcha")
