e8d70fd8140a80848b9c3a1c41f8c5d2d4cc8d6d
monica <macanyiz@gmial.com>
2024-01-11 09:36:49 +0100

    Más cambios

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,31 @@ const app = express(); //Nos trae la app
 
 //Controladores para responder a las peticiones por HTTP
 
+
+app.get('/automovil',(request, response, next) => {
+    console.log("Pasamos por el primer middleware app.get")
+    response.redirect('/coche');
+});
+
+app.use('/coche',(request, response, next) => { 
+    console.log("Peticion recibida"); 
+    next(); 
+}); //Middleware; 
+
+
+app.use('/coche',(request, response, next) => { 
+    console.log("Estamos en el segundo middleware"); 
+    response.send({"message": "ok"});
+}); //Middleware;
+
+app.use('/', (request, response, next) => {
+    console.log("Middleware del final");
+    response.status(404).send({"message": "Mal hecho"});
+}); //Middleware error;
+
+
+
 // FIN
 app.listen(port); //Poner en marcha la app
-console.log("Servidor de la app en marcha")
+console.log("Servidor de la app en marcha");
+
