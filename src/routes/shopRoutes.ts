c9541cdf0f0d95bc4e96ed353423676ab7f40985
monica <macanyiz@gmial.com>
2024-01-15 09:25:38 +0100

    !5 de enero

--- src/routes/shopRoutes.ts
+++ src/routes/shopRoutes.ts
@@ -5,9 +5,11 @@ export const shopRouter = Router();
 
 shopRouter.use('/', (request, response, next) => {
 response.send('<h1>Estamos en la tienda</h1>');
-
-
 });
 shopRouter.get('/saludo', (request, response, next) => {
     response.render("prueba", { nombre: "Monica" }); //Renderiza la vista prueba.ejs
+});
+
+shopRouter.get('/', (request, response, next) => {
+    response.render("shop", { title: 'Monica', prods:[{title:'Tetera'}] });
 });
